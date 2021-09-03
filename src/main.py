85a18d9490698f3574ec0e3d2a41664abd2525dc
AlessandroLiscio <aleliscio97@gmail.com>
2021-09-03 20:44:51 +0200

    lode, lodview, virtuoso, webvowl and docker implemented

--- src/main.py
+++ src/main.py
@@ -15,14 +15,7 @@ Invoke the method convert on the instance of class RMLConverter by:
 
 # ATTRIBUTES
 rml_attribute_file_path = os.path.join('mappings', 'attribute-map.ttl')
-rdf_attribute_graph = rml_converter.convert(rml_attribute_file_path)
-
-# REGIONS
-rml_region_file_path = os.path.join('mappings', 'region-map.ttl')
-rdf_region_graph = rml_converter.convert(rml_region_file_path)
-
-# Merge graphs
-final_graph = rdf_attribute_graph + rdf_region_graph
+g = rml_converter.convert(rml_attribute_file_path)
 
 # Define useful prefixes
 PREFIX_ATTRIBUTES = "http://soilproject/id/skos/sc/"
@@ -33,9 +26,8 @@ PREFIX_TEMP = "https://soilproject/temp#"
 PREFIX_ISPRA_CORE = "http://dati.isprambiente.it/ontology/core#"
 PREFIX_ISPRA_PLACES = "http://dati.isprambiente.it/id/place/"
 
-# Get all the attributes useful information by querying the graph
-attributes_data = pd.read_csv(os.path.join('data', f'Descrizione_campi.csv'), sep=";")
-attributes_data = attributes_data[6:]
+# Get all the indicators useful information
+indicators_data = pd.read_csv(os.path.join('data', f'Descrizione_campi.csv'), sep=";")[6:]
 
 for year in ['2012', '2015']:
     for target in ['Regioni']:
@@ -108,83 +100,64 @@ for year in ['2012', '2015']:
                 # rdfs:label
                 g.add(( 
                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
->>>>>>> 48521f0 (typo fix)
                     URIRef(RDFS.label),
-                    Literal("Consumo del suolo nella regione " + f"{region_name} nell'anno {year}")
-        ))
-        final_graph.add(( 
-                    URIRef(PREFIX_REG_YEAR_COLLECTION + f"REG{region_code}_{year}"),
-                    URIRef(RDF.type),
-                    URIRef(PREFIX_ISPRA_CORE + "IndicatorCollection")
-        ))
-        final_graph.add(( 
-                    URIRef(PREFIX_REG_YEAR_COLLECTION + f"REG{region_code}_{year}"),
+                    Literal(f"{short}{code}_{year}_{indicator}")
+                    ))
+                # rdfs:comment (Description)
+
+                g.add(( 
+                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+                    URIRef(RDFS.comment),
+                    Literal(f"{indicators_data[indicators_data['Campo_ID'] == indicator]['Descrizione'].to_list()[0]} per {name} nell'anno {year}")
+                    ))
+                # dc:isPartOf -> (TARGET-YEAR collection)
+                g.add(( 
+                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+                    URIRef(DC.isPartOf),
+                    URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}")
+                    ))
+                # dc:type
+                g.add(( 
+                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+                    URIRef(DC.type),
+                    URIRef(PREFIX_ATTRIBUTES + indicator.lower())
+                    ))
+                # rdf:value -> from table
+                g.add(( 
+                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+                    URIRef(RDF.value),
+                    Literal(row[indicator])
+                    ))
+                # foaf:PrimaryTopic
+                g.add(( 
+                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
                     URIRef(FOAF.PrimaryTopic),
-                    URIRef(PREFIX_ISPRA_PLACES + region_name_refactored)
-        ))
-        final_graph.add(( 
-                    URIRef(PREFIX_REG_YEAR_COLLECTION + f"REG{region_code}_{year}"),
-                    URIRef(DC.date),
+                    URIRef(PREFIX_ISPRA_PLACES + name_refactored)
+                    ))
+                # dc:date
+                g.add(( 
+                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+                    URIRef(DC.date), 
                     Literal(year)
-        ))
-        
-        for j in range(0, len(attributes_data['Campo_ID'])):
+                    ))
 
-            indicator = attributes_data.iloc[j]['Campo_ID'].lower()
-
-            final_graph.add(( 
-                        URIRef(PREFIX_INDICATORS + f"REG{region_code}_{year}_{indicator}"),
-                        URIRef(RDFS.label),
-                        Literal(f"REG{region_code}_{year}_{indicator.upper()}")
-                        ))
-            index = attributes_data.index[attributes_data['Campo_ID'] == indicator.upper()]
-            if len(index) > 0:
-                index = index[0]-6
-                final_graph.add(( 
-                            URIRef(PREFIX_INDICATORS + f"REG{region_code}_{year}_{indicator}"),
-                            URIRef(RDFS.comment),
-                            Literal(f"{attributes_data.iloc[index]['Descrizione']} per la regione {region_name} nell'anno {year}")
-                        ))
-            final_graph.add(( 
-                        URIRef(PREFIX_INDICATORS + f"REG{region_code}_{year}_{indicator}"),
-                        URIRef(DC.isPartOf),
-                        URIRef(PREFIX_REG_YEAR_COLLECTION + f"REG{region_code}_{year}")
-                        ))
-            final_graph.add(( 
-                        URIRef(PREFIX_INDICATORS + f"REG{region_code}_{year}_{indicator}"),
-                        URIRef(DC.type),
-                        URIRef(PREFIX_ATTRIBUTES + indicator)
-                        ))
-            final_graph.add(( 
-                        URIRef(PREFIX_INDICATORS + f"REG{region_code}_{year}_{indicator}"),
-                        URIRef(RDF.value),
-                        Literal(row[j+2])
-                        ))
-            final_graph.add(( 
-                        URIRef(PREFIX_INDICATORS + f"REG{region_code}_{year}_{indicator}"),
-                        URIRef(FOAF.PrimaryTopic),
-                        URIRef(PREFIX_ISPRA_PLACES + region_name_refactored)
-                        ))
-            final_graph.add(( 
-                        URIRef(PREFIX_INDICATORS + f"REG{region_code}_{year}_{indicator}"),
-                        URIRef(DC.date), 
-                        Literal(year)
-                        ))
+print("graph has {} statements.".format(len(g)))
+g.serialize(destination='output.ttl', format='turtle')
 
 # Print the triples contained into the RDF graph.
-# for s, p, o in final_graph:
+# for s, p, o in g:
 #     print(s, p, o)
-
-final_graph.serialize(destination='output.ttl', format='turtle')
-print("graph has {} statements.".format(len(final_graph)))
-
-# qres = final_graph.query(
+#
+# ###########
+# # QUERIES #
+# ###########
+# qres = g.query(
 #     """SELECT ?name ?ha
 #         WHERE
 #             { ?place rdfs:label ?name ;
 #             <https://lisciofortini/attribute/C1> ?ha .}""")
 # 
-# qres = final_graph.query(
+# qres = g.query(
 #     """SELECT ?name ?d ?v
 #         WHERE
 #             { ?place rdfs:label ?name ;
@@ -192,18 +165,18 @@ print("graph has {} statements.".format(len(final_graph)))
 #                 ?s <http://prova.attribute.ex/att#descrizione> ?d.
 #                 FILTER regex(?name, "^Emilia").}""")
 # 
-# qres = final_graph.query(
+# qres = g.query(
 #     """SELECT ?attr ?d
 #         WHERE
 #             { ?attr <http://prova.attribute.ex/att#descrizione> ?d}""")
 # 
 # # Get all the attributes useful information by querying the graph
-# qres = final_graph.query(
+# qres = g.query(
 #     """SELECT DISTINCT ?descr
 #         WHERE {
 #             ?id <http://prova.attribute.ex/att#descrizione> ?descr
 #         }""")
-
+#
 # print("###################################")
 # for row in qres:
 #     print("%s %s %s" % row)

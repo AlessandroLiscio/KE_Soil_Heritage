85775ae28d938fdf9bd71e13fc7cb892ceb44c13
AlessandroLiscio <aleliscio97@gmail.com>
2021-09-03 21:06:00 +0200

    setup.py fixed

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 install_requires=[
-    'pandas'
+    'pandas',
     'rdflib'
-    'git+https://github.com/anuzzolese/pyrml'
 ]
 
-setup(name='KE_Soil_Heritage', version='1.0.0', packages=find_packages(), install_requires=install_requires)
+setup(name='KE_Soil_Heritage', version='1.0.0', packages=find_packages(),
+      install_requires=install_requires, dependency_links=['https://github.com/anuzzolese/pyrml/tarball/master'])

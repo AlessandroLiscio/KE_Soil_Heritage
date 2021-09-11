b9680228fa3198f5e42646d1d2b007bef8c70419
AlessandroLiscio <aleliscio97@gmail.com>
2021-09-11 16:25:18 +0200

    indicators_graph implementation

--- src/main.py
+++ src/main.py
@@ -5,194 +5,217 @@ from rdflib import Graph
 from rdflib.namespace import RDF, RDFS, SKOS, FOAF, DC
 from rdflib.term import URIRef, Literal
 
-# Create an instance of the class RMLConverter.
-rml_converter = RMLConverter()
-
-'''
-Invoke the method convert on the instance of class RMLConverter by:
- - using the file examples/artist/artist-map.ttl (see the examples in this repo);
- - obtaining an RDF graph as output.
-'''
-
-# Start from protégé graph
-protege_graph = Graph().parse('./data/webprotege.owl')
-
-# ATTRIBUTES
-rml_attribute_file_path = os.path.join('mappings', 'attribute-map.ttl')
-attributes_graph = rml_converter.convert(rml_attribute_file_path)
+#TODO:
+# A good practice for creating IRIs is the following
+#   {protocol}://{domain}/{format}/{enity type}/{entity key}
+#
+# Where:
+# • protocol might be http, https, ftp, etc.
+# • domain is the Web domain where the resource is available
+# • format specifies the medium type. A limited number of values should be used
+#     such as "doc" if the identified resource is a descriptive document,
+#     "set" if the resource is a dataset, "id" or "item" if the
+#     resource is a real world object
+# • entity type refers to the class to which the instance belongs to,
+#     e.g. Person, Organisation, CulturalProperty
+# • entity key denotes a unique alphanumeric identifier of the instance,
+#     e.g. the legal identifier of a public administration agency or
+#     a post code (depending on the nature of the instance).
 
 # Define useful prefixes
-PREFIX_ATTRIBUTES = "https://soilproject.org/onto/id/skos/sc/"
-PREFIX_REG_YEAR_COLLECTION = "https://soilproject.org/onto/id/collection/sc/"
-PREFIX_INDICATORS = "https://soilproject.org/onto/id/indicator/sc/"
-PREFIX_TEMP = "https://soilproject.org/onto/temp#"
+PREFIX_SOILPROJECT = "https://soilproject.org/onto"
 
+PREFIX_ATTRIBUTES = PREFIX_SOILPROJECT+"/attributi"
+PREFIX_REG_YEAR_COLLECTION = PREFIX_SOILPROJECT+"/CollezioneIndicatori"
+PREFIX_INDICATOR = PREFIX_SOILPROJECT+"/Indicatore"
+PREFIX_TEMP = PREFIX_SOILPROJECT+"TEMP#"
+
+#TODO: remove this prefixes from ontology
 PREFIX_ISPRA_CORE = "http://dati.isprambiente.it/ontology/core#"
 PREFIX_ISPRA_PLACES = "http://dati.isprambiente.it/id/place/"
 
-# Get all the indicators useful information
+# start from protégé graph
+protege_graph = Graph().parse('./data/webprotege.owl')
+
+# Create Indicators Ontology
 indicators_data = pd.read_csv(os.path.join('data', f'Descrizione_campi.csv'), sep=";")[6:]
+indicators_graph = Graph()
 
-python_graph = Graph()
-for year in ['2012', '2015']:
-    for target in ['Regioni']:
-    # for target in ['Regioni', 'Province']:
-    # for target in ['Regioni', 'Province', 'Comuni']: # -> CAREFUL: 'Comuni' files are VERY big!
+for i, row in indicators_data.iterrows():
 
-        print(f'Working on {target}_{year}.csv')
-        # Load Data
-        data = pd.read_csv(os.path.join('data', f'{target}_{year}.csv'), sep=";", encoding='latin1') #encoding_errors='ignore')
+    # Store fields in variables
+    indicator_id = row['Campo_ID']
+    indicator_name = row['Campo']
+    indicator_descr = row['Descrizione']
 
-        for i, row in data.iterrows():
+    # Indicator class
+    indicators_graph.add((
+        URIRef(PREFIX_INDICATOR + f"/{indicator_id}"),
+        URIRef(RDF.type),
+        Literal(PREFIX_INDICATOR)
+    ))
+    # Indicator label
+    indicators_graph.add((
+        URIRef(PREFIX_INDICATOR + f"/{indicator_id}"),
+        URIRef(RDFS.label),
+        Literal(indicator_id)
+    ))
+    # # Indicator name
+    # indicators_graph.add((
+    #     URIRef(PREFIX_INDICATOR + f"/{indicator_id}"),
+    #     URIRef(),
+    #     Literal(indicator_name)
+    # ))
+    # Indicator description
+    indicators_graph.add((
+        URIRef(PREFIX_INDICATOR + f"/{indicator_id}"),
+        URIRef(RDFS.comment),
+        Literal(indicator_descr)
+    ))
 
-            # Store target code and name
-            if target == 'Regioni':
-                short = 'REG'
-                long = 'la regione'
-                code = row['COD_REG']
-                name = row['NOME_Regione']
-            elif target == 'Province':
-                short = 'PRO'
-                long = 'la provincia di'
-                code = row['COD_PRO']
-                name = row['NOME_Provincia']
-            else:
-                short = 'COM'
-                long = 'il comune di'
-                code = row['PRO_COM']
-                name = row['NOME_Comune']
-            name_refactored = name.replace(' ','-').lower()
-
-
-            ###########################
-            # REGION-YEAR COLLECTIONS #
-            ###########################
-            # rdfs:label
-            python_graph.add((
-                URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
-                URIRef(RDFS.label),
-                Literal(f"Consumo del suolo per {long} {name} nell'anno {year}")
-                ))
-            # rdfs:type
-            python_graph.add((
-                URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
-                URIRef(RDF.type),
-                URIRef(PREFIX_ISPRA_CORE + "IndicatorCollection")
-                ))
-            # foaf:PrimaryTopic
-            python_graph.add((
-                URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
-                URIRef(FOAF.PrimaryTopic),
-                URIRef(PREFIX_ISPRA_PLACES + name_refactored)
-                ))
-            # dc:date
-            python_graph.add((
-                URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
-                URIRef(DC.date),
-                Literal(year)
-                ))
-            
-            for indicator in indicators_data['Campo_ID']:
-
-                # Avoid 'C7' indicator:
-                # - Regioni -> C7_RM_1956;C7_RM_1989;C7_RM_1998;C7_RM_2008;C7_RM_2013
-                # - Comuni e Province -> Does not exist
-                if indicator == 'C7': continue;
-
-                ##################################
-                # TARGET-YEAR-INDICATOR ENTITIES #
-                ##################################
-                # rdfs:label
-                python_graph.add((
-                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
-                    URIRef(RDFS.label),
-                    Literal(f"{short}{code}_{year}_{indicator}")
-                    ))
-                # rdfs:comment (Description)
-
-                python_graph.add((
-                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
-                    URIRef(RDFS.comment),
-                    Literal(f"{indicators_data[indicators_data['Campo_ID'] == indicator]['Descrizione'].to_list()[0]} per {name} nell'anno {year}")
-                    ))
-                # dc:isPartOf -> (TARGET-YEAR collection)
-                python_graph.add((
-                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
-                    URIRef(DC.isPartOf),
-                    URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}")
-                    ))
-                # dc:type
-                python_graph.add((
-                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
-                    URIRef(DC.type),
-                    URIRef(PREFIX_ATTRIBUTES + indicator.lower())
-                    ))
-                # rdf:value -> from table
-                python_graph.add((
-                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
-                    URIRef(RDF.value),
-                    Literal(row[indicator])
-                    ))
-                # foaf:PrimaryTopic
-                python_graph.add((
-                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
-                    URIRef(FOAF.PrimaryTopic),
-                    URIRef(PREFIX_ISPRA_PLACES + name_refactored)
-                    ))
-                # dc:date
-                python_graph.add((
-                    URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
-                    URIRef(DC.date),
-                    Literal(year)
-                    ))
-
-# Merge graphs
-final_graph = protege_graph + attributes_graph + python_graph
-
+#
+# python_graph = Graph()
+# for year in ['2012', '2015']:
+#     for target in ['Regioni']:
+#     # for target in ['Regioni', 'Province']:
+#     # for target in ['Regioni', 'Province', 'Comuni']: # -> CAREFUL: 'Comuni' files are VERY big!
+#
+#         print(f'Working on {target}_{year}.csv')
+#         # Load Data
+#         data = pd.read_csv(os.path.join('data', f'{target}_{year}.csv'), sep=";", encoding='latin1') #encoding_errors='ignore')
+#
+#         for i, row in data.iterrows():
+#
+#             # Store target code and name
+#             if target == 'Regioni':
+#                 short = 'REG'
+#                 long = 'la regione'
+#                 code = row['COD_REG']
+#                 name = row['NOME_Regione']
+#             elif target == 'Province':
+#                 short = 'PRO'
+#                 long = 'la provincia di'
+#                 code = row['COD_PRO']
+#                 name = row['NOME_Provincia']
+#             else:
+#                 short = 'COM'
+#                 long = 'il comune di'
+#                 code = row['PRO_COM']
+#                 name = row['NOME_Comune']
+#             name_refactored = name.replace(' ','-').lower()
+#
+#
+#             ###########################
+#             # REGION-YEAR COLLECTIONS #
+#             ###########################
+#             # rdfs:label
+#             python_graph.add((
+#                 URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
+#                 URIRef(RDFS.label),
+#                 Literal(f"Consumo del suolo per {long} {name} nell'anno {year}")
+#                 ))
+#             # rdfs:type
+#             python_graph.add((
+#                 URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
+#                 URIRef(RDF.type),
+#                 URIRef(PREFIX_ISPRA_CORE + "IndicatorCollection")
+#                 ))
+#             # foaf:PrimaryTopic
+#             python_graph.add((
+#                 URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
+#                 URIRef(FOAF.PrimaryTopic),
+#                 URIRef(PREFIX_ISPRA_PLACES + name_refactored)
+#                 ))
+#             # dc:date
+#             python_graph.add((
+#                 URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}"),
+#                 URIRef(DC.date),
+#                 Literal(year)
+#                 ))
+#
+#             for indicator in indicators_data['Campo_ID']:
+#
+#                 # Avoid 'C7' indicator:
+#                 # - Regioni -> C7_RM_1956;C7_RM_1989;C7_RM_1998;C7_RM_2008;C7_RM_2013
+#                 # - Comuni e Province -> Does not exist
+#                 if indicator == 'C7': continue;
+#
+#                 ##################################
+#                 # TARGET-YEAR-INDICATOR ENTITIES #
+#                 ##################################
+#                 # rdfs:label
+#                 python_graph.add((
+#                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+#                     URIRef(RDFS.label),
+#                     Literal(f"{short}{code}_{year}_{indicator}")
+#                     ))
+#                 # rdfs:comment (Description)
+#
+#                 python_graph.add((
+#                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+#                     URIRef(RDFS.comment),
+#                     Literal(f"{indicators_data[indicators_data['Campo_ID'] == indicator]['Descrizione'].to_list()[0]} per {name} nell'anno {year}")
+#                     ))
+#                 # dc:isPartOf -> (TARGET-YEAR collection)
+#                 python_graph.add((
+#                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+#                     URIRef(DC.isPartOf),
+#                     URIRef(PREFIX_REG_YEAR_COLLECTION + f"{short}{code}_{year}")
+#                     ))
+#                 # dc:type
+#                 python_graph.add((
+#                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+#                     URIRef(DC.type),
+#                     URIRef(PREFIX_ATTRIBUTES + indicator.lower())
+#                     ))
+#                 # rdf:value -> from table
+#                 python_graph.add((
+#                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+#                     URIRef(RDF.value),
+#                     Literal(row[indicator])
+#                     ))
+#                 # foaf:PrimaryTopic
+#                 python_graph.add((
+#                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+#                     URIRef(FOAF.PrimaryTopic),
+#                     URIRef(PREFIX_ISPRA_PLACES + name_refactored)
+#                     ))
+#                 # dc:date
+#                 python_graph.add((
+#                     URIRef(PREFIX_INDICATORS + f"{short}{code}_{year}_{indicator.lower()}"),
+#                     URIRef(DC.date),
+#                     Literal(year)
+#                     ))
+#
+#
 # Print graph lengths
 print("protege_graph has {} statements.".format(len(protege_graph)))
-print("attributes_graph has {} statements.".format(len(attributes_graph)))
-print("python_graph has {} statements.".format(len(python_graph)))
-print("final_graph has {} statements.".format(len(final_graph)))
+print("indicators_graph has {} statements.".format(len(indicators_graph)))
+# print("python_graph has {} statements.".format(len(python_graph)))
 
+
+# Merge graphs
+final_graph = protege_graph + indicators_graph # + python_graph
+print("final_graph has {} statements.".format(len(final_graph)))
+# final_graph.serialize(destination='./ontologies/final_graph.ttl', format='turtle')
+#
 # Serialize graphs to .ttl files
 os.mkdir('./ontologies')
 protege_graph.serialize(destination='./ontologies/protege_graph.ttl', format='turtle')
-attributes_graph.serialize(destination='./ontologies/attributes_graph.ttl', format='turtle')
-python_graph.serialize(destination='./ontologies/python_graph.ttl', format='turtle')
-# final_graph.serialize(destination='./ontologies/final_graph.ttl', format='turtle')
+indicators_graph.serialize(destination='./ontologies/indicators_graph.ttl', format='turtle')
+# python_graph.serialize(destination='./ontologies/python_graph.ttl', format='turtle')
 
-# Print the triples contained into the RDF graph.
-# for s, p, o in g:
+# # Print the triples contained into the RDF graph.
+# for s, p, o in final_graph:
 #     print(s, p, o)
-#
+
 # ###########
 # # QUERIES #
 # ###########
-# qres = g.query(
+# qres = final_graph.query(
 #     """SELECT ?s ?o
 #         WHERE
 #             { ?s rdfs:label ?o }""")
-# 
-# qres = g.query(
-#     """SELECT ?name ?d ?v
-#         WHERE
-#             { ?place rdfs:label ?name ;
-#                     ?s ?v.
-#                 ?s <http://prova.attribute.ex/att#descrizione> ?d.
-#                 FILTER regex(?name, "^Emilia").}""")
-# 
-# qres = g.query(
-#     """SELECT ?attr ?d
-#         WHERE
-#             { ?attr <http://prova.attribute.ex/att#descrizione> ?d}""")
-# 
-# # Get all the attributes useful information by querying the graph
-# qres = g.query(
-#     """SELECT DISTINCT ?descr
-#         WHERE {
-#             ?id <http://prova.attribute.ex/att#descrizione> ?descr
-#         }""")
 #
 # print("###################################")
 # for row in qres:
